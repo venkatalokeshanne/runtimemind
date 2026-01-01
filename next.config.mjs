cac4572f4d21142235b14aa6c9f540351c617673
Venkata Lokesh Anne <annevlokesh@gmail.com>
2026-01-01 22:26:03 +0100

    Fix avatar

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+      },
     ],
   },
 };
